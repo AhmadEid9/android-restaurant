fcd7e87dcc13d6d0b4238c2eec2b6c001a331200
AhmadEid9 <ahmad.eid.2.4.99@gmail.com>
2025-02-17 20:55:01 +0200

    fixing binding class in Settings activity

--- app/src/main/java/com/example/restaurant/SettingsActivity.java
+++ app/src/main/java/com/example/restaurant/SettingsActivity.java
@@ -4,26 +4,29 @@ import android.os.Bundle;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
-import androidx.appcompat.app.AppCompatActivity;;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.restaurant.databinding.ActivitySettingsBinding;
 
 
 public class SettingsActivity extends AppCompatActivity {
-//    SettingsActivityBinding binding;
+    ActivitySettingsBinding binding;
     RadioGroup pageGroup, langGroup;
     RadioButton engLanguageRB, frLanguageRB, addPageRB, showPageRB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        binding = ActivitySettingsBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        setContentView(binding.getRoot());
 
-        engLanguageRB = findViewById(R.id.language_eng);
-        frLanguageRB = findViewById(R.id.language_fr);
+        engLanguageRB = binding.languageEng;//findViewById(R.id.language_eng);
+        frLanguageRB = binding.languageFr;//findViewById(R.id.language_fr);
 
-        addPageRB = findViewById(R.id.page_add);
-        showPageRB = findViewById(R.id.page_show);
+        addPageRB = binding.pageAdd;//findViewById(R.id.page_add);
+        showPageRB = binding.pageShow;//findViewById(R.id.page_show);
 
-        pageGroup = findViewById(R.id.page_group);
-        langGroup = findViewById(R.id.language_group);
+        pageGroup = binding.pageGroup;//findViewById(R.id.page_group);
+        langGroup = binding.languageGroup;//findViewById(R.id.language_group);
         if (SettingPref.getStartPage(this).equals(getString(R.string.showing_page))){
             pageGroup.check(R.id.page_show);
         } else {
